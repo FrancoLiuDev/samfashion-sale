09e5236f1429eebea9cb3863c89287920cfdee53
franco <francoliu1979@gmail.com>
2018-06-02 13:18:51 +0800

    test time api

--- app/api/MainSevice.js
+++ app/api/MainSevice.js
@@ -1,73 +1,81 @@
-
 const MscOrderController = require(__controllers + '/msc/MscOrderController')
 const mscOrderController = new MscOrderController()
 
 function MainSevice(app) {
-     
-    app.post('/msc/api/v1/order/', async function(req, res) {
-		console.log('post order',req.body)
-	    updata = req.body
-		mscOrderController
-			.createRetrieveOrder('1', {
-				saleMember:updata.saleMember,
-				orderPhone:updata.orderPhone,
-				orderAddr:updata.orderAddr,
-				orderSpec:updata.orderSpec,
-				orderPrice:updata.orderPrice,
-				orderOther:updata.orderOther,
-			})
-			.then(result => {
-                console.log('ok')
-				res.status(result.status).send(result)
-			})
-			.catch(err => {
-				console.log('err',err)
-				res.status(err.status).send(err)
-			})
-	})
-	app.put('/msc/api/v1/order/', async function(req, res) {
-		console.log('get list ',req.body)
-		var condition = req.body
-		mscOrderController
-			.readOrderList(condition)
-			.then(result => {
-                console.log('ok')
-				res.status(result.status).send(result)
-			})
-			.catch(err => {
-				console.log('err',err)
-				res.status(err.status).send(err)
-				// if (appUtils.isControllerException(err)) {
-				// 	console.log('order/:id', err)
-				// 	res.status(err.status).send(err)
-				// } else {
-				// 	console.log(err)
-				// 	res.status(500).send(err)
-				// }
-			})
-	})
-    app.delete('/msc/api/v1/order/:id', async function(req, res) {	 
-		var id = req.params.id
-		console.log('delete',id)
-		mscOrderController
-			.deleteOrderById(id)
-			.then(result => {
-                console.log('ok')
-				res.status(result.status).send(result)
-			})
-			.catch(err => {
-				console.log('err',err)
-				res.status(err.status).send(err)
-				// if (appUtils.isControllerException(err)) {
-				// 	console.log('order/:id', err)
-				// 	res.status(err.status).send(err)y
-				// } else {
-				// 	console.log(err)
-				// 	res.status(500).send(err)
-				// }
-			})
-	})
-	 
+
+  app.post('/msc/api/v1/order/', async function (req, res) {
+    console.log('post order', req.body)
+    updata = req.body
+    mscOrderController
+      .createRetrieveOrder('1', {
+        saleMember: updata.saleMember,
+        orderPhone: updata.orderPhone,
+        orderAddr: updata.orderAddr,
+        orderSpec: updata.orderSpec,
+        orderPrice: updata.orderPrice,
+        orderOther: updata.orderOther,
+      })
+      .then(result => {
+        console.log('ok')
+        res.status(result.status).send(result)
+      })
+      .catch(err => {
+        console.log('err', err)
+        res.status(err.status).send(err)
+      })
+  })
+  app.put('/msc/api/v1/order/', async function (req, res) {
+    console.log('get list ', req.body)
+    var condition = req.body
+    mscOrderController
+      .readOrderList(condition)
+      .then(result => {
+        console.log('ok')
+        res.status(result.status).send(result)
+      })
+      .catch(err => {
+        console.log('err', err)
+        res.status(err.status).send(err)
+        // if (appUtils.isControllerException(err)) {
+        // 	console.log('order/:id', err)
+        // 	res.status(err.status).send(err)
+        // } else {
+        // 	console.log(err)
+        // 	res.status(500).send(err)
+        // }
+      })
+  })
+  app.delete('/msc/api/v1/order/:id', async function (req, res) {
+    var id = req.params.id
+    console.log('delete', id)
+    mscOrderController
+      .deleteOrderById(id)
+      .then(result => {
+        console.log('ok')
+        res.status(result.status).send(result)
+      })
+      .catch(err => {
+        console.log('err', err)
+        res.status(err.status).send(err)
+        // if (appUtils.isControllerException(err)) {
+        // 	console.log('order/:id', err)
+        // 	res.status(err.status).send(err)y
+        // } else {
+        // 	console.log(err)
+        // 	res.status(500).send(err)
+        // }
+      })
+  })
+  app.get('/msc/api/v1/timezone', async function (req, res) {
+
+    var nowtime = new Date()
+    var offset = new Date().getTimezoneOffset();
+    //return  nowtime.toLocaleString()
+
+    res.status(200).send(nowtime.getMonth() + 1 + "/" + nowtime.getDate() + " " + nowtime.getHours() + ":" + nowtime.getMinutes() + " " + Intl.DateTimeFormat().resolvedOptions().timeZone)
+
+  })
+
 }
 
 module.exports = MainSevice
